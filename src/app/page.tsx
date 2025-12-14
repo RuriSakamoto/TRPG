7fb179996fa9ac572aebfe21be4a29d9ad551fa2
RuriSakamoto <hekisuisyo1028@gmail.com>
2025-12-14 15:49:45 +0900

    不足ファイルの追加

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { GameScreen } from '../components/GameScreen';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-900">
+      <GameScreen />
+    </main>
+  );
+}
